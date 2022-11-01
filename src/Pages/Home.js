a8a1c16dfa23f3adf09e91fd97b38a2640021cc2
Joao Andrade Jr <fullservicemanaus@gmail.com>
2022-11-01 11:31:55 -0400

    add: css in home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import allCharacters from '../data';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <main>
-        {
-          allCharacters.map((chacarter, index) => (
-            <Link to={ `/character/${chacarter.id}` } key={ `${index}${chacarter.fullName}` }>
-              {chacarter.fullName}
-            </Link>
-          ))
-        }
+      <main className="main-container">
+        <section className="link-container">
+          {
+            allCharacters.map((chacarter, index) => (
+              <button
+                type="button"
+                key={ `${index}${chacarter.fullName}` }
+                className="button"
+                onClick={ () => history.push(`/character/${chacarter.id}`) }
+              >
+                { chacarter.fullName }
+              </button>
+              // <Link
+              //   to={ `/character/${chacarter.id}` }
+              //   key={ `${index}${chacarter.fullName}` }
+              //   className="link"
+              // >
+              //   {chacarter.fullName}
+              // </Link>
+            ))
+          }
+        </section>
       </main>
     );
   }
